5882feb00286a416c5a38bd762fc9bdbf84fddcc
Ricky Ybarra <rdybarra@gmail.com>
2015-01-24 17:47:55 -0800

    Changing the production client url to use a non-www version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # The url of the app that hits this API
-  config.client_url = 'http://www.proverbstagger.com'
+  config.client_url = 'http://proverbstagger.com'
 end
